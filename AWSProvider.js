542431967617cb0dd146f693eca391994fb92040
heberline <heber009@hotmail.com>
2019-12-06 22:39:07 -0300

    versao 2

--- AWSProvider.js
+++ AWSProvider.js
@@ -3,139 +3,15 @@ AWS.config.update({ region: 'sa-east-1' });
 
 const isLocal = process.env['TODOMANAGERLOCAL']
 if (!isLocal) {
-AWS.config.update({
-credentials: {
-accessKeyId: 'AKIATLL5ELE6TW7DR3NV',
-secretAccessKey: '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'
-}
-});
+	AWS.config.update({
+		credentials: {
+			accessKeyId: 'AKIATLL5ELE63XN6DWNT',
+			secretAccessKey: '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'
+		}
+	});
 } else {
-AWS.config.update({
-endpoint: 'http://localhost:8000'
-});
+	AWS.config.update({
+		endpoint: 'http://localhost:8000'
+	});
 }
 module.exports = AWS;
-
-AWS.config.update({
-endpoint: 'http://localhost:8000'
-});
-module.exports = AWS;
-• Crie o arquivo taskDAO.js
-const AWS = require('./AWSProvider');
-const uuid = require('uuid');
-const dynamoDB = new AWS.DynamoDB();
-const init = (callback) => {
-dynamoDB.listTables({}, (err, data) => {
-if (err) {
-callback(err, null);
-} else {
-if (data.TableNames.indexOf('Tasks') == -1) {
-dynamoDB.createTable({
-TableName: 'Tasks',
-AttributeDefinitions: [
-{ AttributeName: 'id', AttributeType: 'S' },
-],
-KeySchema: [
-{ AttributeName: 'id', KeyType: 'HASH' },
-],
-ProvisionedThroughput: {
-ReadCapacityUnits: 1,
-WriteCapacityUnits: 1
-},
-}, (err, data) => {
-callback(err, data);
-})
-} else {
-callback(err, { message: 'ok' });
-}
-}
-});
-	}
-const insert = (task, callback) => {
-const id = task.id || uuid();
-dynamoDB.putItem({
-TableName: 'Tasks',
-Item: {
-"id": { S: id },
-"title": { S: task.title },
-"description": { S: task.description },
-"isPriority": { BOOL: task.isPriority },
-"isDone": { BOOL: task.isDone }
-}
-}, (err, data) => {
-if (err) {
-callback(err, null)
-} else {
-callback(null, {
-id,
-title: task.title,
-description: task.description,
-isPriority: task.isPriority,
-isDone: task.isDone
-})
-}
-});
-}
-const listAll = (callback) => {
-dynamoDB.scan({ TableName: 'Tasks' }, (err, data) => {
-if (err) {
-callback(err, null);
-} else {
-const list = [];
-data.Items.forEach(item => {
-const task = {
-id: item.id.S,
-title: item.title.S,
-description: item.description.S,
-isPriority: item.isPriority.BOOL,
-isDone: item.isDone.BOOL,
-}
-list.push(task);
-});
-callback(null, list);
-}
-});
-}
-const findTaskById = (id, callback) => {
-dynamoDB.getItem({
-TableName: 'Tasks',
-Key: {
-	"id": { S: id }
-}
-}, (err, data) => {
-if (err) {
-callback(err, null);
-} else {
-const item = data.Item;
-var task = null;
-if (item) {
-task = {
-id: item.id.S,
-title: item.title.S,
-description: item.description.S,
-isPriority: item.isPriority.BOOL,
-isDone: item.isDone.BOOL,
-}
-}
-callback(null, task)
-}
-});
-}
-const remove = (id, callback) => {
-dynamoDB.deleteItem({
-TableName: 'Tasks',
-Key: {
-"id": { S: id }
-}
-}, (err, data) => {
-if (err) {
-callback(err, null);
-} else {
-callback(null, {
-message: 'deleted'
-});
-}
-})
-}
-
-module.exports = { insert, listAll, findTaskById, remove, init }
--- server.js
+++ server.js
@@ -3,107 +3,118 @@ const app = express();
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const taskDAO = require('./taskDAO');
+
 const SEGREDO = 'euvoupracasa';
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 function cobrarTokenJWT(req, resp, next) {
-if (req.url == '/login') {
-next();
-}
-var token = req.headers['x-access-token'];
-try {
-jwt.verify(token, SEGREDO);
-next();
-} catch (e) {
-resp.status(500).send({ message: 'token invalido' })
-}
+	if (req.url == '/login') {
+		next();
+	}
+	
+	var token = req.headers['x-access-token'];
+	try {
+		jwt.verify(token, SEGREDO);
+		next();
+	} catch (e) {
+		resp.status(500).send({ message: 'token invalido' })
+	}
 }
 app.use(cobrarTokenJWT);
+
 app.get('/', (req, resp) => {
-resp.send('resposta');
+	resp.send('resposta');
 });
+
 app.post('/tasks', (request, response) => {
-const body = request.body;
-const task = {
-title: body.title,
-description: body.description,
-isDone: body.isDone,
-isPriority: body.isPriority
-};
-taskDAO.insert(task, (err, data) => {
-if (err) {
-response.status(500).send(err);
-} else {
-response.send(data);
-}
+	const body = request.body;
+	const task = {
+		title: body.title,
+		description: body.description,
+		isDone: body.isDone,
+		isPriority: body.isPriority
+	};
+	taskDAO.insert(task, (err, data) => {
+		if (err) {
+			response.status(500).send(err);
+		} else {
+			response.send(data);
+		}
+	});
 });
-});
+
 app.get('/tasks', (request, response) => {
-taskDAO.listAll((err, data) => {
-if (err) {
-response.status(500).send(err);
-} else {
-response.send(data);
-}
+	taskDAO.listAll((err, data) => {
+		if (err) {
+			response.status(500).send(err);
+		} else {
+			response.send(data);
+		}
+	});
 });
-});
+
 app.get('/tasks/:taskId', (request, response) => {
-taskDAO.findTaskById(request.params.taskId, (err, task) => {
-if (task) {
-response.status(200);
-response.send(task);
-	} else if (err) {
-response.status(500);
-response.send(err);
-} else {
-response.status(404);
-response.send();
-}
+	taskDAO.findTaskById(request.params.taskId, (err, task) => {
+		if (task) {
+			response.status(200);
+			response.send(task);
+		} else if (err) {
+			response.status(500);
+			response.send(err);
+		} else {
+			response.status(404);
+			response.send();
+		}
+	});
 });
-});
+
 app.put('/tasks/:taskId', (request, response) => {
-const body = request.body;
-const task = {
-id: request.params.taskId,
-title: body.title,
-description: body.description,
-isDone: body.isDone,
-isPriority: body.isPriority
-};
-taskDAO.insert(task, (err, data) => {
-if (err) {
-response.status(500).send(err);
-} else {
-response.send(task);
-}
+	const body = request.body;
+	const task = {
+		id: request.params.taskId,
+		title: body.title,
+		description: body.description,
+		isDone: body.isDone,
+		isPriority: body.isPriority
+	};
+	taskDAO.insert(task, (err, data) => {
+		if (err) {
+			response.status(500).send(err);
+		} else {
+			response.send(task);
+		}
+	});
 });
-});
+
 app.delete('/tasks/:taskId', (request, response) => {
-taskDAO.remove(request.params.taskId, (err, data) => {
-if (err) {
-response.status(500).send(err);
-} else {
-response.status(200).send(data);
-}
+	taskDAO.remove(request.params.taskId, (err, data) => {
+		if (err) {
+			.status(500).send(err);
+		} else {
+			response.status(200).send(data);
+		}
+	});
 });
-});
+
 app.post('/login', (req, resp) => {
-var body = req.body;
-if (body.username == 'usuario' && body.password == 'teste123') {
-var token = jwt.sign({ username: 'usuario', role: 'admin' }, SEGREDO, {
-expiresIn: '1h'
-});
-resp.send({ auth: true, token });
-} else {
-resp.status(403).send({ auth: false, message: 'usuario invalido' });
-}
+	var body = req.body;
+	if (body.username == 'usuario' && body.password == 'teste123') {
+		var token = jwt.sign({ username: 'usuario', role: 'admin' }, SEGREDO, {
+			expiresIn: '1h'
+		});
+		resp.send({ auth: true, token });
+	} else {
+		resp.status(403).send({ auth: false, message: 'usuario invalido' });
+	}
 })
 taskDAO.init((err, data) => {
-if (err) {
-console.log('Servidor nao iniciado por erro', err);
-} else {
-app.listen(3000, () => {
-console.log('Servidor iniciado');
-});
-}
+	if (err) {
+		console.log('Servidor nao iniciado por erro', err);
+	} else {
+		app.listen(3000, () => {
+			console.log('Servidor iniciado');
+		});
+	}
 });
